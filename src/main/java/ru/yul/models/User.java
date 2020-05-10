f8de41c9ddf1fda7c51588225886168c12cdea86
mrpokerface <imrpokerface@gmail.com>
2020-05-10 13:20:19 +0300

    modified application.properties to connect to the database

--- src/main/java/ru/yul/models/User.java
+++ src/main/java/ru/yul/models/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_pkey")
     private Long id;
     private String deviceId;
 
